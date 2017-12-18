bd873f1f3981c528acc34cd5a46e8116e06f7ff8
FABAME <FABAME@DESKTOP-89D0BT8>
2017-12-17 22:32:15 -0500

    El proyecto se ha generado correctamente.

--- FirmadorPensemosSI/src/pensemos/firmador/FirmarPDF.java
+++ FirmadorPensemosSI/src/pensemos/firmador/FirmarPDF.java
@@ -7,21 +7,25 @@ import java.security.PrivateKey;
 import java.security.Provider;
 import java.security.cert.X509Certificate;
 
-public class FirmarPDF
-{
-  public void firmarPDF(X509Certificate x509, PrivateKey privKey, Provider provider, InputStream entrada, ByteArrayOutputStream salida, String firmaTSA, String datosTSA)
-    throws UnknownError, Exception
-  {
-    //CMS.iniciarComponente();
-    CMS cms = new CMS(entrada);
-    CMS.setFuenteHorariaLocal();
-    System.out.println(firmaTSA);
-    if (firmaTSA.equalsIgnoreCase("true"))
+public class FirmarPDF {
+
+    public void firmarPDF(X509Certificate x509, PrivateKey privKey, Provider provider, InputStream entrada, ByteArrayOutputStream salida, String firmaTSA, String datosTSA)
+            throws UnknownError, Exception {
+        //CMS.iniciarComponente();
+        CMS cms = new CMS(entrada);
+        CMS.setFuenteHorariaLocal();
+        System.out.println(firmaTSA);
+        /* if (firmaTSA.equalsIgnoreCase("true"))
     {
       String[] tsa = datosTSA.split("--");
       CMS.setFuenteHorariaTSA(tsa[0], tsa[1], tsa[2]);
-    }
-    cms.firmar(x509, privKey, provider, salida);                  
+    }*/
+        if (firmaTSA.equalsIgnoreCase("true")) {
+            String[] tsa = datosTSA.split("--");
+            //CMS.setFuenteHorariaTSA(tsa[0], tsa[1], tsa[2]);
+            CMS.setFuenteHorariaSNTP(tsa[0]);
+        }
+        cms.firmar(x509, privKey, provider, salida);
 
-  }
+    }
 }
